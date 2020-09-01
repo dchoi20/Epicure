b356fe6834dbfea314571ac06291594bbe808d78
Danny Choi <choi.danny20@gmail.com>
2020-09-01 11:34:04 -0500

    added a get request for search api for zomato

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,39 @@
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(data => {
+  $.get("/api/user_data").then((data) => {
     $(".member-name").text(data.email);
+  });
+
+  let coords = document.querySelector("#get-location");
+  // let categoryBtn = document.querySelector(".category");
+  coords.addEventListener("click", function() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      console.log(lat);
+      console.log(long);
+
+      fetch(
+        `https://developers.zomato.com/api/v2.1/cuisines?lat=${lat}&lon=${long}`,
+        {
+          method: "get",
+          headers: {
+            Accept: "application/json",
+            "user-key": "4319a1eed8d7dbe3e7050e70e4bcaf33",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          for (let i = 0; i < result.cuisines.length; i++) {
+            console.log(result.cuisines[i]);
+            console.log(result.cuisines[i].cuisine.cuisine_name);
+            $(
+              `<button class="category">${result.cuisines[i].cuisine.cuisine_name}</button><br>`
+            ).appendTo("#cuisines");
+          }
+        });
+    });
   });
 });
