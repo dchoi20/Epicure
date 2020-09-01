66f2405c4d5d73b72a9dac3efb7239a099795688
Danny Choi <choi.danny20@gmail.com>
2020-09-01 09:11:27 -0500

    updated libraries

--- public/js/login.js
+++ public/js/login.js
@@ -18,23 +18,23 @@ $(document).ready(() => {
     }
 
     // If we have an email and password we run the loginUser function and clear the form
-    // loginUser(userData.email, userData.password);
-    // emailInput.val("");
-    // passwordInput.val("");
+    loginUser(userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
   });
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  //   function loginUser(email, password) {
-  //     $.post("/api/login", {
-  //       email: email,
-  //       password: password,
-  //     })
-  //       .then(() => {
-  //         window.location.replace("/members");
-  //         // If there's an error, log the error
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //       });
-  //   }
+  function loginUser(email, password) {
+    $.post("/api/login", {
+      email: email,
+      password: password,
+    })
+      .then(() => {
+        window.location.replace("/members");
+        // If there's an error, log the error
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
