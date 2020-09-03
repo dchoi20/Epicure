8bae5e8a37becf371a0d72510ffed71b2d318aea
Danny Choi <choi.danny20@gmail.com>
2020-09-03 14:17:41 -0500

    added map api to reviews page. new overview.js file for reviews page

---
+++ public/js/overview.js
@@ -0,0 +1,35 @@
+let params = new URL(document.location).searchParams;
+let restaurantId = params.get("restaurant");
+$(document).ready(() => {
+  console.log(restaurantId);
+
+  fetch(
+    `https://developers.zomato.com/api/v2.1/restaurant?res_id=${restaurantId}`,
+    {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "user-key": "4319a1eed8d7dbe3e7050e70e4bcaf33",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      console.log(result.location.address);
+      console.log(result.location.longitude);
+      let long = result.location.longitude;
+      console.log(result.location.latitude);
+      let lat = result.location.latitude;
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoiZGNob2kyMCIsImEiOiJja2VpcmFyeXYxYTY5MnJzNHBmcGV2OW5rIn0.m4KpGy6Glb5cbcAeJowlkQ";
+      var map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+        center: [long, lat], // starting position [lng, lat]
+        zoom: 15, // starting zoom
+      });
+
+      var marker = new mapboxgl.Marker().setLngLat([long, lat]).addTo(map);
+    });
+});
--- public/js/restaurantReview.js
+++ public/js/restaurantReview.js
@@ -1,124 +1,121 @@
-
-
-
 const $reviewTitle = $(".review-title");
 const $reviewText = $(".review-textarea");
 const $saveReviewBtn = $(".save-review");
-const $newReviewBtn = $(".new-review")
+const $newReviewBtn = $(".new-review");
 const $reviewList = $(".list-container .list-group");
 
 let activeReview = {};
 
 const getReview = () => {
-    return $.ajax({
-        url: "",
-        method: "GET",
-    });
+  return $.ajax({
+    url: "",
+    method: "GET",
+  });
 };
 
 const saveReview = (review) => {
-    return $.ajax({
-        url: "",
-        data: review,
-        method: "POST",
-    });
+  return $.ajax({
+    url: "",
+    data: review,
+    method: "POST",
+  });
 };
 
 const deleteReview = (id) => {
-    return $.ajax({
-        url: "" + id,
-        method: "DELETE",
-    });
+  return $.ajax({
+    url: "" + id,
+    method: "DELETE",
+  });
 };
 
 const renderActiveReview = () => {
-    $saveReviewBtn.hide();
+  $saveReviewBtn.hide();
 
-    if (activeReview.id) {
-        $reviewTitle.attr("readonly", true);
-        $reviewText.attr("readonly", true);
-        $reviewTitle.val(activeReview.title);
-        $reviewText.val(activeReview.text);
-    } else {
-        $reviewTitle.attr("readonly", false);
-        $reviewText.attr("readonly", false);
-        $reviewTitle.val("");
-        $reviewText.val("");
-
-    }
+  if (activeReview.id) {
+    $reviewTitle.attr("readonly", true);
+    $reviewText.attr("readonly", true);
+    $reviewTitle.val(activeReview.title);
+    $reviewText.val(activeReview.text);
+  } else {
+    $reviewTitle.attr("readonly", false);
+    $reviewText.attr("readonly", false);
+    $reviewTitle.val("");
+    $reviewText.val("");
+  }
 };
 
-const handleReviewSave = function () {
-    const newReview = {
-        title: $reviewTitle.val(),
-        text: $reviewText.val(),
-    };
-    saveReview(newReview).then(() => {
-        getAndRenderReviews();
-        renderActiveReview();
-    });
-};
-
-const handleReviewDelete = function (event) {
-    event.stopPropagation();
-
-    const review = $(this).parent(".list-group-item").data();
-
-    if (activeReview.id === review.id) {
-        activeReview = {};
-    }
-    deleteReview(review.id).then(() => {
-        getAndRenderReviews();
-        renderActiveReview();
-    });
-};
-
-const handleReviewView = function () {
-    activeReview = $(this).data();
+const handleReviewSave = function() {
+  const newReview = {
+    title: $reviewTitle.val(),
+    text: $reviewText.val(),
+  };
+  saveReview(newReview).then(() => {
+    getAndRenderReviews();
     renderActiveReview();
+  });
 };
 
-const handleRenderSaveBtn = function () {
-    if (!$reviewTitle.val().trim() || !$reviewText.val().trim()) {
-        $saveReviewBtn.hide();
-    } else {
-        $saveReviewBtn.show();
-    }
+const handleReviewDelete = function(event) {
+  event.stopPropagation();
+
+  const review = $(this)
+    .parent(".list-group-item")
+    .data();
+
+  if (activeReview.id === review.id) {
+    activeReview = {};
+  }
+  deleteReview(review.id).then(() => {
+    getAndRenderReviews();
+    renderActiveReview();
+  });
+};
+
+const handleReviewView = function() {
+  activeReview = $(this).data();
+  renderActiveReview();
+};
+
+const handleRenderSaveBtn = function() {
+  if (!$reviewTitle.val().trim() || !$reviewText.val().trim()) {
+    $saveReviewBtn.hide();
+  } else {
+    $saveReviewBtn.show();
+  }
 };
 
 const renderReviewList = (reviews) => {
-    $reviewList.empty();
+  $reviewList.empty();
 
-    const reviewListItems = [];
+  const reviewListItems = [];
 
-    const create$li = (text, withDeleteButton = true) => {
-        const $li = $("<li class=`list-group-item`>");
-        const $span = $("<span>").text(text);
-        $li.append($span);
+  const create$li = (text, withDeleteButton = true) => {
+    const $li = $("<li class=`list-group-item`>");
+    const $span = $("<span>").text(text);
+    $li.append($span);
 
-        if (withDeleteButton) {
-            const $delBtn = $(
-                "<i class=`fas fa-trash-alt float-right text-danger delete-note'>"
-            );
-            $li.append($delBtn);
-        }
-        return $li;
-
-    };
-    if (reviews.length === 0) {
-        reviewListItems.push(create$li("No saved Reviews", false));
+    if (withDeleteButton) {
+      const $delBtn = $(
+        "<i class=`fas fa-trash-alt float-right text-danger delete-note'>"
+      );
+      $li.append($delBtn);
     }
+    return $li;
+  };
+  if (reviews.length === 0) {
+    reviewListItems.push(create$li("No saved Reviews", false));
+  }
 
-    reviews.forEach((review) => {
-        const $li = create$li(review.title).data(review);
-        reviewListItems.push($li);
-    });
+  reviews.forEach((review) => {
+    const $li = create$li(review.title).data(review);
+    reviewListItems.push($li);
+  });
 
-    $reviewList.append(reviewListItems);
+  $reviewList.append(reviewListItems);
 };
 
 const getAndRenderReviews = () => {
-    return getReview().then(renderReviewList);
+  return getReview().then(renderReviewList);
 };
 
 $saveReviewBtn.on("click", handleReviewSave);
